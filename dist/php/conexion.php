6817c33918a2fcd3cb9b7f2dd298fa1dd2190901
David Guillen <dguillen@arkusnexus.com>
2016-11-28 12:22:53 -0800

    Conexion.php fixed

--- dist/php/conexion.php
+++ dist/php/conexion.php
@@ -1,6 +1,8 @@
 <?php
-	header('Content-type: text/plain; charset=utf-8');
-	 function getEstado($value) {
+header('Content-type: text/plain; charset=utf-8');
+date_default_timezone_set('America/Tijuana');
+
+function getEstado($value) {
 	switch ((int)$value) {
  		case 1:
           return 'En espera';
@@ -15,13 +17,22 @@
 	$user = 'u657840993_pi';
 	$pw	  = 'escolarespi';
 	$db   = 'u657840993_newpi';
-			$conn = mysqli_connect($host,$user,$pw,$db);
-			$conn->set_charset("utf8");
-			date_default_timezone_set('America/Tijuana');
-//	if (mysqli_connect_errno()){
-//			echo"DB False" . mysqli_connect_error();
-//		}else{
-//			echo"DB True";
-//		}
 
+	$conn = new mysqli($host,$user,$pw,$db);
+
+	$conn->set_charset("utf8");
+
+
+		/*
+		 * Use this instead of $connect_error if you need to ensure
+		 * compatibility with PHP versions prior to 5.2.9 and 5.3.0.
+
+		if (mysqli_connect_error()) {
+		    die('Connect Error (' . mysqli_connect_errno() . ') '
+		            . mysqli_connect_error());
+		}
+		else{	
+			echo 'Success... ' . $mysqli->host_info . "\n";
+
+			} */
 ?> 
